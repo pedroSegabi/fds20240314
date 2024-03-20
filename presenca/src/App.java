facbf5bced48b09ef894618a1e516f67b37be247
Pedro Ernesto Segabinazzi Viganico <144963322+pedroSegabi@users.noreply.github.com>
2024-03-19 21:55:01 -0300

    Update App.java

--- presenca/src/App.java
+++ presenca/src/App.java
@@ -1,5 +1,7 @@
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Aula de FDS!");
+
+        System.out.println("PedroSegabi - 19/03/2024");
     }
 }
